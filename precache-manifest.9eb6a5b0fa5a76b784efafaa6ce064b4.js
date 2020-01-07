0c448cef3dcb8290818aa5d5b299fb94b2c9af29
RoccoDev <roccodev.business@gmail.com>
2020-01-07 17:18:42 +0100

    Updates

---
+++ precache-manifest.9eb6a5b0fa5a76b784efafaa6ce064b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5eaf1c8738ae6a9417f643b511e2d87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4b409dd83c20a06a6b1",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d649c293e6b634249183",
+    "url": "./static/js/2.aadf2f27.chunk.js"
+  },
+  {
+    "revision": "7ad9f179c67b37adf3f46d2d13c80ac7",
+    "url": "./static/js/2.aadf2f27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4b409dd83c20a06a6b1",
+    "url": "./static/js/main.a3296c6e.chunk.js"
+  },
+  {
+    "revision": "5ac319ea6108da8db6b5",
+    "url": "./static/js/runtime-main.0067032d.js"
+  }
+]);
